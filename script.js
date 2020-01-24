ff1b5531c288b66daa6b8f18fbe9c05789ae5ab2
Gavin Quirk <gavinquirk@gmail.com>
2020-01-24 14:29:22 -0800

    detect edges, connect particles, generate particles based on device width

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-let p;
+const particles = [];
 
 // Setup runs once when file is loaded
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  p = new Particle();
+  const particlesLength = Math.floor(window.innerWidth / 10);
+
+  for (let i = 0; i < particlesLength; i++) {
+    particles.push(new Particle());
+  }
 }
 
 // Draw runs continuously to create animations
 function draw() {
   background(55, 100, 144);
-  p.update();
-  p.draw();
+  particles.forEach((p, index) => {
+    p.update();
+    p.draw();
+    p.checkParticles(particles.slice(index));
+  });
 }
 
 class Particle {
@@ -24,13 +31,38 @@ class Particle {
     this.size = 10;
   }
 
+  // Update movement by adding velocity
   update() {
     this.pos.add(this.vel);
+    this.edges();
   }
 
+  // Draw single particle
   draw() {
     noStroke();
     fill('rgba(255, 255, 255, 0.5)');
     circle(this.pos.x, this.pos.y, this.size);
+  }
+
+  // Detect edges
+  edges() {
+    if (this.pos.x < 0 || this.pos.x > width) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y < 0 || this.pos.y > height) {
+      this.vel.y *= -1;
+    }
+  }
+
+  // Connect particles
+  checkParticles(particles) {
+    particles.forEach(particle => {
+      const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+
+      if (d < 120) {
+        stroke('rgba(255, 255, 255, 0.1)');
+        line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+      }
+    });
   }
 }
